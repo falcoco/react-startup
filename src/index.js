4c0aafd86d7b2a03be1d1f4cc3fb0d297c92aee2
falcoe <greatyhh@gmail.com>
2018-09-27 23:32:18 +0800

    🐶Controlled component

--- src/index.js
+++ src/index.js
@@ -16,27 +16,39 @@ class ShoppingList extends React.Component{
 }
 
 class Square extends React.Component{
-    constructor(props){
-     super(props);
-     this.state = {
-         value: props.value,
-     };
-    }
     render(){
         return (
             <button
                 className="square"
-                onClick={()=>this.setState({value: 'X'})}
+                onClick={()=>this.props.onClick()}
             >
-                {this.state.value}
+                {this.props.value}
             </button>
         );
     }
 }
 
 class Board extends React.Component{
+    constructor(props){
+       super(props);
+       this.state = {
+           squares: Array(9).fill(null),
+       };
+    }
+
+    handleClick(i){
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
     renderSquare(i){
-        return <Square value={i}/>;
+        return (
+            <Square
+                value={ this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
     }
 
     render() {
@@ -84,5 +96,3 @@ ReactDOM.render(
     <Game/>,
     document.getElementById('root')
 )
-
-
